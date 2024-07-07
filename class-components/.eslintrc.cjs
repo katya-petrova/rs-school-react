1e4f2f5c1d96e181b7949d922b7348966653a05e
Katsiaryna Piatrova <katsiaryna_piatrova@epam.com>
2024-07-07 14:34:40 +0400

    update .eslintrc

--- class-components/.eslintrc.cjs
+++ class-components/.eslintrc.cjs
@@ -16,6 +16,11 @@ module.exports = {
       jsx: true,
     },
   },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   rules: {
     "react-refresh/only-export-components": [
       "warn",

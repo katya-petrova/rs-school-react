6769f9e9504c42d33721ec0cac8f1b4ab185641f
Katsiaryna Piatrova <katsiaryna_piatrova@epam.com>
2024-07-30 17:38:10 +0300

    migrate to next.js

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,27 @@ import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import NotFoundPage from './views/NotFoundPage/NotFoundPage';
 import { ThemeContext } from './context/ThemeContext';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const App: React.FC = () => {
   const [theme, setTheme] = useState('dark');
 
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <Router>
-        <ErrorBoundary>
-          <div className="App">
-            <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </div>
-        </ErrorBoundary>
-      </Router>
-    </ThemeContext.Provider>
+    <Provider store={store}>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <Router>
+          <ErrorBoundary>
+            <div className="App">
+              <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            </div>
+          </ErrorBoundary>
+        </Router>
+      </ThemeContext.Provider>
+    </Provider>
   );
 };
 export default App;
